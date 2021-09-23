82f2da6b89336794ce7fa319f327e9c0fb9b960a
xiaowei <xiaowei20150509YIUHLK>
2021-09-23 18:04:08 +0800

    录屏测试实例，保存MP4文件到本地

--- recorder/recorder/main.cpp
+++ recorder/recorder/main.cpp
@@ -1,10 +1,13 @@
 #include "recorderWidget.h"
 #include <QtWidgets/QApplication>
 
+#include "mainwindow.h"	
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    recorder w;
+    //recorder w;
+	MainWindow w;
     w.show();
     return a.exec();
 }
